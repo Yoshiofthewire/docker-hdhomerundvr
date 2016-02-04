16adf68bee949ebad42b5483665b556dd70ce763
Yoshiofthewire <mbeacher@gmail.com>
2016-02-04 18:59:36 -0500

    fix it
    I'm an idiot

--- install.sh
+++ install.sh
@@ -32,8 +32,8 @@ cat <<'EOT' > /etc/service/hdhomerun/run
 umask 000
 [[ ! -f /opt/hdhomerun/hdhomerun_record_x64 ]] && cp /tmp/hdhomerun_record_x64 /opt/hdhomerun/
 chown -R nobody:users /opt/hdhomerun
-exec /sbin/setuser nobody /opt/hdhomerun/hdhomerun_record_x64 status
 exec /sbin/setuser nobody /opt/hdhomerun/hdhomerun_record_x64 start
+exec /sbin/setuser nobody /opt/hdhomerun/hdhomerun_record_x64 version
 EOT
 
 chmod -R +x /etc/service/ /etc/my_init.d/
